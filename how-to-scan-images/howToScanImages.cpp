4fe7a1ba07b55cf323a7f6b9980097e730fb8e1c
Tom Lyons <tbl3rd@gmail.com>
2013-10-23 16:03:24 -0400

    Simplify.

--- how-to-scan-images/howToScanImages.cpp
+++ how-to-scan-images/howToScanImages.cpp
@@ -1,5 +1,6 @@
 ﻿#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include <opencv2/ts/ts_perf.hpp>
 #include <iostream>
 #include <sstream>
 
@@ -15,6 +16,8 @@ static void showUsage(const char *av0)
         << "Usage: " << av0 << " <image-file> <divisor> [G]"
         << std::endl << std::endl
         << "Where: <image-file> is the path to an image file."
+        << std::endl
+        << "       The image should have a Mat::depth() of CV_8U."
         << std::endl
         << "       <divisor> is a small integer less than 255."
         << std::endl
@@ -48,36 +51,70 @@ static int useCommandLine(int ac, const char *av[], cv::Mat &img)
 }
 
 
-static cv::Mat &ScanImageAndReduceArrayOp(cv::Mat &I, const uchar table[])
+// Call scan(*this) a lot and report the average run time in milliseconds.
+//
+// After cloning image, scan() visits every element, reduces it according
+// to table or lookupTable, and returns the result.
+//
+// Each test() runs scan() many times and reports its average run time.
+//
+typedef struct Test {
+    const cv::Mat &table;
+    const cv::Mat &image;
+    const char *const label;
+    cv::Mat (*scan)(const struct Test &);
+
+    void operator()(void) const {
+        static const int runs = 100;
+        const double tickZero = cv::getTickCount();
+        for (int i = 0; i < runs; ++i) const cv::Mat ignore = (*scan)(*this);
+        const double ticks = cv::getTickCount() - tickZero;
+        const double totalSeconds = ticks / cv::getTickFrequency();
+        const double msPerRun = totalSeconds * 1000 / runs;
+        std::cout << "Average time to reduce with " << label << ": "
+                  << msPerRun << " milliseconds." << std::endl;
+    }
+
+    Test(const cv::Mat &lut, const cv::Mat &i, const char *m,
+         cv::Mat (*s)(const struct Test &)):
+        table(lut), image(i), label(m), scan(s) {}
+} Test;
+
+
+static cv::Mat scanWithArrayOp(const Test &t)
 {
-    CV_Assert(CV_8U == I.depth());
-    int nRows = I.rows;
-    int nCols = I.cols * I.channels();
-    if (I.isContinuous()) {
+    CV_Assert(CV_8U == t.image.depth());
+    cv::Mat image = t.image.clone();
+    int nRows = image.rows;
+    int nCols = image.cols * image.channels();
+    if (image.isContinuous()) {
         nCols *= nRows;
         nRows = 1;
     }
+    const uchar *const table = t.table.data;
     for (int i = 0; i < nRows; ++i) {
-        uchar *const p = I.ptr<uchar>(i);
+        uchar *const p = image.ptr<uchar>(i);
         for (int j = 0; j < nCols; ++j) p[j] = table[p[j]];
     }
-    return I;
+    return image;
 }
 
 
-static cv::Mat &ScanImageAndReduceIterator(cv::Mat &I, const uchar table[])
+static cv::Mat scanWithMatIter(const Test &t)
 {
-    CV_Assert(CV_8U == I.depth());
-    switch (I.channels()) {
+    CV_Assert(CV_8U == t.image.depth());
+    cv::Mat image = t.image.clone();
+    const uchar *const table = t.table.data;
+    switch (image.channels()) {
     case 1: {
-        cv::MatIterator_<uchar> it = I.begin<uchar>();
-        const cv::MatIterator_<uchar> end = I.end<uchar>();
+        cv::MatIterator_<uchar> it = image.begin<uchar>();
+        const cv::MatIterator_<uchar> end = image.end<uchar>();
         for ( ; it != end; ++it) *it = table[*it];
         break;
     }
     case 3: {
-        cv::MatIterator_<cv::Vec3b> it = I.begin<cv::Vec3b>();
-        const cv::MatIterator_<cv::Vec3b> end = I.end<cv::Vec3b>();
+        cv::MatIterator_<cv::Vec3b> it = image.begin<cv::Vec3b>();
+        const cv::MatIterator_<cv::Vec3b> end = image.end<cv::Vec3b>();
         for( ; it != end; ++it) {
             (*it)[0] = table[(*it)[0]];
             (*it)[1] = table[(*it)[1]];
@@ -85,122 +122,65 @@ static cv::Mat &ScanImageAndReduceIterator(cv::Mat &I, const uchar table[])
         }
     }
     }
-    return I;
+    return image;
 }
 
 
-static cv::Mat &ScanImageAndReduceRandom(cv::Mat &I, const uchar *const table)
+static cv::Mat scanWithAt(const Test &t)
 {
-    CV_Assert(CV_8U == I.depth());
-    switch (I.channels()) {
+    CV_Assert(CV_8U == t.image.depth());
+    cv::Mat image = t.image.clone();
+    const uchar *const table = t.table.data;
+    switch (image.channels()) {
     case 1: {
-        for (int i = 0; i < I.rows; ++i) {
-            for (int j = 0; j < I.cols; ++j) {
-                I.at<uchar>(i,j) = table[I.at<uchar>(i,j)];
+        for (int i = 0; i < image.rows; ++i) {
+            for (int j = 0; j < image.cols; ++j) {
+                image.at<uchar>(i,j) = table[image.at<uchar>(i,j)];
             }
         }
         break;
     }
     case 3: {
-        cv::Mat_<cv::Vec3b> _I = I;
-        for (int i = 0; i < I.rows; ++i) {
-            for (int j = 0; j < I.cols; ++j) {
+        cv::Mat_<cv::Vec3b> _I = image;
+        for (int i = 0; i < image.rows; ++i) {
+            for (int j = 0; j < image.cols; ++j) {
                 _I(i,j)[0] = table[_I(i,j)[0]];
                 _I(i,j)[1] = table[_I(i,j)[1]];
                 _I(i,j)[2] = table[_I(i,j)[2]];
             }
         }
-        I = _I;
+        image = _I;
         break;
     }
     }
-    return I;
+    return image;
 }
 
 
-static double reduceWithArrayOp(const cv::Mat &I, const uchar table[])
+static cv::Mat scanWithLut(const Test &t)
 {
-    static const int times = 100;
-    double t = cv::getTickCount();
-    for (int i = 0; i < times; ++i) {
-        cv::Mat J = I.clone();
-        J = ScanImageAndReduceArrayOp(J, table);
-    }
-    t = 1000 * ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    t /= times;
-    return t;
-}
-
-
-static double reduceWithMatIter(const cv::Mat &I, const uchar table[])
-{
-    static const int times = 100;
-    double t = cv::getTickCount();
-    for (int i = 0; i < times; ++i) {
-        cv::Mat J = I.clone();
-        J = ScanImageAndReduceIterator(J, table);
-    }
-    t = 1000 * ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    t /= times;
-    return t;
-}
-
-
-static double reduceWithAt(const cv::Mat &I, const uchar table[])
-{
-    static const int times = 100;
-    double t = cv::getTickCount();
-    for (int i = 0; i < times; ++i) {
-        cv::Mat J = I.clone();
-        J = ScanImageAndReduceRandom(J, table);
-    }
-    t = 1000 * ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    t /= times;
-    return t;
-}
-
-
-static double reduceWithLookupTable(const cv::Mat &I, const uchar table[])
-
-{
-    static const int times = 100;
-    cv::Mat J, lookUpTable(1, 256, CV_8U);
-    uchar *const p = lookUpTable.data;
-    for (int i = 0; i < 256; ++i) p[i] = table[i];
-    double t = cv::getTickCount();
-    for (int i = 0; i < times; ++i) {
-        cv::Mat J = I.clone();
-        LUT(I, lookUpTable, J);
-    }
-    t = 1000 * ((double)cv::getTickCount() - t) / cv::getTickFrequency();
-    t /= times;
-    return t;
+    CV_Assert(CV_8U == t.image.depth());
+    cv::Mat J = t.image.clone();
+    LUT(t.image, t.table, J);
+    return J;
 }
 
 
 int main(int ac, const char *av[])
 {
-    cv::Mat I;
-    const int divisor = useCommandLine(ac, av, I);
+    cv::Mat image;
+    const int divisor = useCommandLine(ac, av, image);
     if (divisor == 0) return 1;
-    uchar table[256];
-    cv::Mat lookUpTable(1, 256, CV_8U);
-    uchar *const p = lookUpTable.data;
-    const int tableSize = sizeof table / sizeof table[0];
-    for (int i = 0; i < tableSize; ++i) {
-        p[i] = table[i] = (divisor * (i / divisor));
-    }
-    double t = reduceWithArrayOp(I, table);
-    std::cout << "Average time to reduce with operator[]:  "
-              << t << " milliseconds."<< std::endl;
-    t = reduceWithMatIter(I, table);
-    std::cout << "Average time to reduce with MatIterator: "
-              << t << " milliseconds."<< std::endl;
-    t = reduceWithAt(I, table);
-    std::cout << "Average time to reduce with at():        "
-              << t << " milliseconds."<< std::endl;
-    t = reduceWithLookupTable(I, table);
-    std::cout << "Average time to reduce with LUT():       "
-              << t << " milliseconds."<< std::endl;
+    cv::Mat table(1, 256, CV_8U);
+    uchar *const p = table.data;
+    for (int i = 0; i < table.cols; ++i) p[i] = (divisor * (i / divisor));
+    const Test tests[] = {
+        Test(table, image, "operator[]", &scanWithArrayOp),
+        Test(table, image, "iterator",   &scanWithMatIter),
+        Test(table, image, "at()",       &scanWithAt),
+        Test(table, image, "LUT()",      &scanWithLut)
+    };
+    const int testsCount = sizeof tests / sizeof tests[0];
+    for (int i = 0; i < testsCount; ++i) (tests[i])();
     return 0;
 }
