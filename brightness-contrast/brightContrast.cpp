850cd28a308a198d90fd7b64366037be30dcadd7
Tom Lyons <tbl3rd@gmail.com>
2013-10-24 21:01:20 -0400

    Bus checkpoint that will likely crash MacOSX.

--- brightness-contrast/brightContrast.cpp
+++ brightness-contrast/brightContrast.cpp
@@ -37,12 +37,51 @@ static cv::Mat gainBias(const cv::Mat &input, double alpha, int beta)
     for (int i = 0; i < head.rows; ++i) {
         for (int j = 0; j < head.cols; ++j) {
             for (int c = 0; c < 3; ++c) {
-                const int resultAtIjc = alpha * head(i,j)[c] + beta;
-                result(i,j)[c] = cv::saturate_cast<uchar>(resultAtIjc);
+                const int atIjc = alpha * head(i,j)[c] + beta;
+                result(i,j)[c] = cv::saturate_cast<uchar>(atIjc);
             }
         }
     }
     return result;
+}
+
+static cv::Mat gainBiasAt(const cv::Mat &input, double alpha, int beta)
+{
+    cv::Mat result = cv::Mat::zeros(input.size(), input.type());
+    for (int i = 0; i < input.rows; ++i) {
+        for (int j = 0; j < input.cols; ++j) {
+            for (int c = 0; c < 3; ++c) {
+                const int atIjc = alpha * input.at<cv::Vec3b>(i,j)[c] + beta;
+                result.at<cv::Vec3b>(i,j)[c] = cv::saturate_cast<uchar>(atIjc);
+            }
+        }
+    }
+    return result;
+}
+
+static cv::Mat withConvertTo(const cv::Mat &input, double alpha, int beta)
+{
+    cv::Mat result;
+    input.convertTo(result, -1, alpha, beta);
+    return result;
+}
+
+typedef cv::Mat (*LinearTransform)(const cv::Mat &, double, int);
+
+static void applyTransform(const cv::Mat &input, LinearTransform lt)
+{
+    static const double alphaMax = 2.0;
+    static const int betaMax = 100;
+    static const int max = 10;
+    for (int i = 0; i <= max; ++i) {
+        const double alpha = 1.0 + alphaMax * i / max;
+        for (int j = 0; j <= max; ++j) {
+            const int beta = 0 + betaMax * j / max;
+            std::stringstream ss; ss << alpha << ":" << beta << std::ends;
+            cv::Mat gb = (*lt)(input, alpha, beta);
+            cv::imshow(ss.str(), gb); cv::waitKey(50);
+        }
+    }
 }
 
 int main(int ac, const char *av[])
@@ -50,18 +89,11 @@ int main(int ac, const char *av[])
     const cv::Mat input = useCommandLine(ac, av);
     if (!input.data) return 1;
     cv::imshow(av[1], input); cv::waitKey(50);
-    static const int max = 10;
-    static const double alphaMax = 2.0;
-    static const int betaMax = 100;
-    for (int i = 0; i <= max; ++i) {
-        const double alpha = 1.0 + alphaMax * i / max;
-        for (int j = 0; j <= max; ++j) {
-            const int beta = 0 + betaMax * j / max;
-            std::stringstream ss; ss << alpha << ":" << beta << std::ends;
-            cv::Mat gb = gainBias(input, alpha, beta);
-            cv::imshow(ss.str(), gb); cv::waitKey(50);
-        }
-    }
+    static LinearTransform lts[] = {
+        &gainBias, &gainBiasAt, &withConvertTo
+    };
+    static const int ltsCount = sizeof lts / sizeof lts[0];
+    for (int i = 0; i < ltsCount; ++i) applyTransform(input, lts[i]);
     cv::waitKey(0);
     return 0;
 }
