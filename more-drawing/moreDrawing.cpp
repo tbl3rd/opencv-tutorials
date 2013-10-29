6690872bbed0331895e6ddeeaa6d81298be4412a
Tom Lyons <tbl3rd@gmail.com>
2013-10-29 15:48:42 -0400

    Document more parameters.

--- more-drawing/moreDrawing.cpp
+++ more-drawing/moreDrawing.cpp
@@ -2,9 +2,14 @@
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
 
+// The default line type 8 means 8-connected Bresenham.
+// LINETYPE = 4 means 4-connected Bresenham
+// LINETYPE = CV_AA means anti-alias the line
+//
 static const int LINETYPE = 8;
-static const int NUMBER = 255;
 
+// A single source of randomness for this program.
+//
 static cv::RNG rng(0xffffffff);
 
 // Show image and return true if program should stop.
@@ -23,6 +28,25 @@ static cv::Scalar randomColor(void)
     const char green = schar(rng);
     const char red   = schar(rng);
     return cv::Scalar(blue, green, red);
+}
+
+// Return a random font face.  This is only for documentation because for
+// all i, i == face[i].  This could just return the index, in other words.
+//
+static int randomFontFace(void)
+{
+    static const int face[] = {
+        [0] = cv::FONT_HERSHEY_SIMPLEX,
+        [1] = cv::FONT_HERSHEY_PLAIN,
+        [2] = cv::FONT_HERSHEY_DUPLEX,
+        [3] = cv::FONT_HERSHEY_COMPLEX,
+        [4] = cv::FONT_HERSHEY_TRIPLEX,
+        [5] = cv::FONT_HERSHEY_COMPLEX_SMALL,
+        [6] = cv::FONT_HERSHEY_SCRIPT_SIMPLEX,
+        [7] = cv::FONT_HERSHEY_SCRIPT_COMPLEX
+    };
+    const int index = rng.uniform(0, 8);
+    return face[index];
 }
 
 // Return a random point from a larger region centered around image.
@@ -44,20 +68,21 @@ static cv::Point randomPoint(const cv::Mat &image)
 //
 static int randomLines(cv::Mat &image)
 {
-    for (int i = 0; i < NUMBER; ++i) {
+    for (int i = 0; i < UCHAR_MAX; ++i) {
+        const int thickness = rng.uniform(1, 10);
         cv::line(image, randomPoint(image), randomPoint(image),
-                 randomColor(), rng.uniform(1, 10), 8);
+                 randomColor(), thickness, LINETYPE);
         if (showImage(image)) return true;
     }
     return false;
 }
 
-// Draw random rectangles on image.
+// Draw random rectangles on image.  Fill about 1/3 of them.
 //
 static int randomRectangles(cv::Mat &image)
 {
-    for (int i = 0; i < NUMBER; ++i) {
-        const int thickness = MAX(rng.uniform(-3, 10), -1);
+    for (int i = 0; i < UCHAR_MAX; ++i) {
+        const int thickness = MAX(rng.uniform(-3, 10), CV_FILLED);
         cv::rectangle(image, randomPoint(image), randomPoint(image),
                       randomColor(), thickness, LINETYPE);
         if (showImage(image)) return true;
@@ -65,34 +90,36 @@ static int randomRectangles(cv::Mat &image)
     return false;
 }
 
-// Draw random elliptical arcs on image.
+// Draw random 5/6 open elliptical arcs (some filled) on image.
 //
 static int randomEllipticArcs(cv::Mat &image)
 {
-    for (int i = 0; i < NUMBER; ++i) {
+    for (int i = 0; i < UCHAR_MAX; ++i) {
         const cv::Point center = randomPoint(image);
         const cv::Size axes(rng.uniform(0, 200), rng.uniform(0, 200));
         const double angle = rng.uniform(0, 180);
+        const int thickness = rng.uniform(-1, 9);
         cv::ellipse(image, center, axes, angle, angle - 100, angle + 200,
-                    randomColor(), rng.uniform(-1, 9), LINETYPE);
+                    randomColor(), thickness, LINETYPE);
         if (showImage(image)) return true;
     }
     return false;
 }
 
-// Draw two random triangles on image.
+// Draw two random triangles of varying thickness on image.
 //
 static int randomTriangles(cv::Mat &image)
 {
     static const int vertexCount = 3;
     static const int polyCount = 2;
-    for (int i = 0; i< NUMBER; ++i) {
+    for (int i = 0; i< UCHAR_MAX; ++i) {
         const cv::Point points[polyCount][vertexCount] = {
             randomPoint(image), randomPoint(image), randomPoint(image),
             randomPoint(image), randomPoint(image), randomPoint(image)
         };
         const cv::Point *curves[polyCount] = { points[0], points[1] };
         const int vertexCounts[polyCount] = { vertexCount, vertexCount };
+        const int thickness = rng.uniform(1, 10);
         cv::polylines(image, curves, vertexCounts, polyCount, true,
                       randomColor(), rng.uniform(1, 10), LINETYPE);
         if (showImage(image)) return true;
@@ -106,7 +133,7 @@ static int randomFilledTriangles(cv::Mat &image)
 {
     static const int vertexCount = 3;
     static const int polyCount = 2;
-    for (int i = 0; i < NUMBER; ++i) {
+    for (int i = 0; i < UCHAR_MAX; ++i) {
         const cv::Point points[polyCount][vertexCount] = {
             randomPoint(image), randomPoint(image), randomPoint(image),
             randomPoint(image), randomPoint(image), randomPoint(image)
@@ -123,24 +150,27 @@ static int randomFilledTriangles(cv::Mat &image)
 //
 static int randomCircles(cv::Mat &image)
 {
-    for (int i = 0; i < NUMBER; ++i) {
+    for (int i = 0; i < UCHAR_MAX; ++i) {
         const cv::Point center = randomPoint(image);
-        cv::circle(image, center, rng.uniform(0, 300), randomColor(),
-                   rng.uniform(-1, 9), LINETYPE);
+        const int radius = rng.uniform(0, 300);
+        const int thickness = rng.uniform(-1, 9);
+        cv::circle(image, center, radius, randomColor(), thickness, LINETYPE);
         if (showImage(image)) return true;
     }
     return false;
 }
 
-// Render text in random styles on image.
+// Render text in random styles at random points on image.
 //
 static int randomText(cv::Mat &image)
 {
-    for (int i = 1; i < NUMBER; ++i) {
+    static const char msg[] = "Testing text rendering";
+    for (int i = 1; i < UCHAR_MAX; ++i) {
         const cv::Point origin = randomPoint(image);
-        cv::putText(image, "Testing text rendering", origin, rng.uniform(0, 8),
-                    0.05 * rng.uniform(0, 100) + 0.1, randomColor(),
-                    rng.uniform(1, 10), LINETYPE);
+        const double scale = 0.1 + 0.05 * rng.uniform(0, 100);
+        const int thickness = rng.uniform(1, 10);
+        cv::putText(image, msg, origin, randomFontFace(), scale,
+                    randomColor(), thickness, LINETYPE);
         if (showImage(image)) return true;
     }
     return false;
@@ -151,15 +181,18 @@ static int randomText(cv::Mat &image)
 //
 static int bigFinale(cv::Mat &image)
 {
-    static const int font = cv::FONT_HERSHEY_COMPLEX;
     static const char msg[] = "OpenCV forever!";
-    const cv::Size textSize = cv::getTextSize(msg, font, 3.0, 5, 0);
-    const cv::Size size = image.size() - textSize;
+    static const int face = cv::FONT_HERSHEY_COMPLEX;
+    static const double scale = 3.0;
+    static const int thickness = 5;
+    static const cv::Size msgSize
+        = cv::getTextSize(msg, face, scale, thickness, 0);
+    const cv::Size size = image.size() - msgSize;
     const cv::Point origin(size.width / 2, size.height / 2);
-    for (int i = 0; i < NUMBER; i += 2) {
+    for (int i = 0; i < UCHAR_MAX; ++i) {
         const cv::Scalar color(i, i, 255);
         cv::Mat fade = image - cv::Scalar::all(i);
-        cv::putText(fade, msg, origin, font, 3, color, 5, LINETYPE);
+        cv::putText(fade, msg, origin, face, scale, color, thickness, LINETYPE);
         if (showImage(fade)) return true;
     }
     return false;
