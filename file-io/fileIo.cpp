931718c16dfadaa0c74e1f60139bd38d1a23e312
Tom Lyons <tbl3rd@gmail.com>
2013-10-30 12:43:06 -0400

    More descriptive and more consistent names.

--- file-io/fileIo.cpp
+++ file-io/fileIo.cpp
@@ -66,90 +66,97 @@ static void read(const cv::FileNode &node, SomeData &x,
 
 static std::ostream &operator<<(std::ostream &out, const SomeData &m)
 {
-    out << "{" << "aString" << " = " << m.aString << ", "
-        << ""  << "aDouble" << " = " << m.aDouble << ", "
-        << ""  << "anInt"   << " = " << m.anInt   << "}";
+    out << "anInt"   << "="         << m.anInt
+        << ", "
+        << "aDouble" << "="         << m.aDouble
+        << ", "
+        << "aString" << "=" << "\"" << m.aString << "\"";
+
     return out;
+}
+
+static bool writeSomeData(const char *filename)
+{
+    std::cout << "Writing ... ";
+    cv::Mat ucharEye = cv::Mat_<uchar>::eye(3, 3);
+    cv::Mat doubleZeros = cv::Mat_<double>::zeros(3, 1);
+    SomeData someData(1);
+    cv::FileStorage fs(filename, cv::FileStorage::WRITE);
+    fs << "iterationNr" << 100
+       << "strings"
+       << "[" << "image1.jpg" << "Awesomeness" << "baboon.jpg" << "]"
+       << "mapping" << "{" << "One" << 1 << "Two" << 2 << "}"
+       << "ucharEye" << ucharEye << "doubleZeros" << doubleZeros
+       << "someData" << someData;
+    fs.release();
+    std::cout << "done." << std::endl;
+    return true;
+}
+
+static bool readSomeData(const char *filename)
+{
+    std::cout << std::endl << "Reading: " << std::endl;
+    cv::FileStorage fs(filename, cv::FileStorage::READ);
+
+    //fs["iterationNr"] >> itNr;
+    const int itNr = fs["iterationNr"];
+    std::cout << itNr << std::endl;
+    if (!fs.isOpened()) {
+        std::cerr << "Failed to open " << filename << std::endl;
+        return false;
+    }
+
+    const cv::FileNode strings = fs["strings"];
+    if (strings.type() != cv::FileNode::SEQ) {
+        std::cerr << "strings is not a sequence! FAIL" << std::endl;
+        return false;
+    }
+
+    const cv::FileNodeIterator it_end = strings.end();
+    for (cv::FileNodeIterator it = strings.begin(); it != it_end; ++it) {
+        const std::string &s = *it;
+        // std::cout << (std::string)*it << std::endl;
+        std::cout << "\"" << s << "\"" << std::endl;
+    }
+
+    const cv::FileNode mapping = fs["mapping"];
+    std::cout << "Two  " << (int)(mapping["Two"]) << "; ";
+    std::cout << "One  " << (int)(mapping["One"]) << std::endl << std::endl;
+
+
+    SomeData someData;
+    cv::Mat ucharEye, doubleZeros;
+
+    fs["ucharEye"] >> ucharEye;
+    fs["doubleZeros"] >> doubleZeros;
+    fs["someData"] >> someData;
+
+    std::cout << std::endl
+              << "ucharEye = "    << std::endl << ucharEye    << std::endl
+              << "doubleZeros = " << std::endl << doubleZeros << std::endl
+              << std::endl
+              << "someData: " << someData << std::endl
+              << std::endl;
+
+    std::cout << "Read NonExisting resulting in default data." << std::endl;
+    fs["NonExisting"] >> someData;
+    std::cout << "someData: " << someData << std::endl;
+    return true;
 }
 
 int main(int ac, char *av[])
 {
-    if (ac != 2) {
-        showUsage(av[0]);
-        return 1;
-    }
-
-    std::string filename = av[1];
-
-    { //write
-        cv::Mat R = cv::Mat_<uchar>::eye(3, 3);
-        cv:: Mat T = cv::Mat_<double>::zeros(3, 1);
-        SomeData m(1);
-        cv::FileStorage fs(filename, cv::FileStorage::WRITE);
-        fs << "iterationNr" << 100
-           << "strings"
-           << "[" << "image1.jpg" << "Awesomeness" << "baboon.jpg" << "]"
-           << "Mapping" << "{" << "One" << 1 << "Two" << 2 << "}"
-           << "R" << R << "T" << T
-           << "SomeData" << m;
-        fs.release();
-        std::cout << "Write Done." << std::endl;
-    }
-
-    {//read
-        std::cout << std::endl << "Reading: " << std::endl;
-        cv::FileStorage fs;
-        fs.open(filename, cv::FileStorage::READ);
-
-        //fs["iterationNr"] >> itNr;
-        const int itNr = fs["iterationNr"];
-        std::cout << itNr;
-        if (!fs.isOpened()) {
-            std::cerr << "Failed to open " << filename << std::endl;
-            showUsage(av[0]);
-            return 1;
-        }
-
-        cv::FileNode n = fs["strings"];
-        if (n.type() != cv::FileNode::SEQ) {
-            std::cerr << "strings is not a sequence! FAIL" << std::endl;
-            return 1;
-        }
-
-        cv::FileNodeIterator const it_end = n.end();
-        for (cv::FileNodeIterator it = n.begin(); it != it_end; ++it) {
-            std::cout << (std::string)*it << std::endl;
-        }
-
-        n = fs["Mapping"];
-        std::cout << "Two  " << (int)(n["Two"]) << "; ";
-        std::cout << "One  " << (int)(n["One"]) << std::endl << std::endl;
-
-
-        SomeData m;
-        cv::Mat R, T;
-
-        fs["R"] >> R;
-        fs["T"] >> T;
-        fs["SomeData"] >> m;
-
+    const bool ok
+        =  ac == 2
+        && writeSomeData(av[1])
+        && readSomeData(av[1]);
+    if (ok) {
         std::cout << std::endl
-                  << "R = " << R << std::endl
-                  << "T = " << T << std::endl
-                  << std::endl
-                  << "SomeData = " << std::endl << m << std::endl
+                  << "Tip: Open " << av[0]
+                  << " with a text editor to see the serialized data."
                   << std::endl;
-
-        std::cout << "Attempt to read NonExisting "
-                  << "(should initialize the data structure with its default).";
-        fs["NonExisting"] >> m;
-        std::cout << std::endl << "NonExisting = "
-                  << std::endl << m << std::endl;
+        return 0;
     }
-
-    std::cout << std::endl
-              << "Tip: Open up " << filename
-              << " with a text editor to see the serialized data." << std::endl;
-
-    return 0;
+    showUsage(av[0]);
+    return 1;
 }
