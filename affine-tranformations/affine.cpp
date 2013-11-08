40e5dc8965e7d5bfc61b28aaacb988ffdbd5e3ba
Tom Lyons <tbl3rd@gmail.com>
2013-11-08 12:24:21 -0500

    Factor out the warp code.

--- affine-tranformations/affine.cpp
+++ affine-tranformations/affine.cpp
@@ -28,37 +28,37 @@ static void makeWindow(const char *window, const cv::Mat &image, int reset = 0)
 }
 
 
+static cv::Mat warpImage(const cv::Mat &src)
+{
+    cv::Mat result = cv::Mat::zeros(src.size(), src.type());
+    const cv::Point2f srcTri[3] = {
+        cv::Point2f(0,            0           ),
+        cv::Point2f(src.cols - 1, 0           ),
+        cv::Point2f(0,            src.rows - 1)
+    };
+    const cv::Point2f dstTri[3] = {
+        cv::Point2f(src.cols * 0.00, src.rows * 0.33),
+        cv::Point2f(src.cols * 0.85, src.rows * 0.25),
+        cv::Point2f(src.cols * 0.15, src.rows * 0.70)
+    };
+    const cv::Mat warpMat = cv::getAffineTransform(srcTri, dstTri);
+    cv::warpAffine(src, result, warpMat, result.size());
+    return result;
+}
+
+
 static void showTransformations(const cv::Mat &src)
 {
-    cv::Point2f srcTri[3];
-    cv::Point2f dstTri[3];
-
     cv::Mat rot_mat(2, 3, CV_32FC1);
-    cv::Mat warp_mat(2, 3, CV_32FC1);
-    cv::Mat warp_dst, warp_rotate_dst;
+    cv::Mat warp_rotate_dst;
 
     /// Set the dst image the same type and size as src
-    warp_dst = cv::Mat::zeros(src.rows, src.cols, src.type());
-
-    /// Set your 3 points to calculate the  Affine Transform
-    srcTri[0] = cv::Point2f(0,0);
-    srcTri[1] = cv::Point2f(src.cols - 1, 0);
-    srcTri[2] = cv::Point2f(0, src.rows - 1);
-
-    dstTri[0] = cv::Point2f(src.cols * 0.0, src.rows * 0.33);
-    dstTri[1] = cv::Point2f(src.cols * 0.85, src.rows * 0.25);
-    dstTri[2] = cv::Point2f(src.cols * 0.15, src.rows * 0.7);
-
-    /// Get the Affine Transform
-    warp_mat = cv::getAffineTransform(srcTri, dstTri);
-
-    /// Apply the Affine Transform just found to the src image
-    cv::warpAffine(src, warp_dst, warp_mat, warp_dst.size());
+    const cv::Mat warpDst = warpImage(src);
 
     /** Rotating the image after Warp */
 
     /// Compute a rotation matrix with respect to the center of the image
-    cv::Point center(warp_dst.cols / 2, warp_dst.rows / 2);
+    cv::Point center(warpDst.cols / 2, warpDst.rows / 2);
     double angle = -50.0;
     double scale = 0.6;
 
@@ -66,11 +66,11 @@ static void showTransformations(const cv::Mat &src)
     rot_mat = cv::getRotationMatrix2D(center, angle, scale);
 
     /// Rotate the warped image
-    cv::warpAffine(warp_dst, warp_rotate_dst, rot_mat, warp_dst.size());
+    cv::warpAffine(warpDst, warp_rotate_dst, rot_mat, warpDst.size());
 
     /// Show what you got
     makeWindow("Source image", src);
-    makeWindow("Warp", warp_dst);
+    makeWindow("Warp", warpDst);
     makeWindow("Warp+Rotate", warp_rotate_dst);
 }
 
