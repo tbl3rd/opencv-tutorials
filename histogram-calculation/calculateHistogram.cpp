7cbce269bf48487aa23416b4eed52eada311a419
Tom Lyons <tbl3rd@gmail.com>
2013-11-12 12:32:57 -0500

    Another factoring checkpoint.

--- histogram-calculation/calculateHistogram.cpp
+++ histogram-calculation/calculateHistogram.cpp
@@ -27,6 +27,52 @@ static void makeWindow(const char *window, const cv::Mat &image, int reset = 0)
     ++moveCount;
 }
 
+// Return a histogram with histSize bins for the color values in plane.
+//
+static cv::Mat_<float> calculatePlane(const cv::Mat &plane, int histSize)
+{
+    static const int    imageCount     = 1;
+    static const int    dimensionCount = 1;
+    static const int    histSizes[]    = { histSize };
+    static const float  histRange[]    = { 0, histSize };
+    static const float *histRanges[]   = { histRange };
+    static const bool   uniform        = true;
+    static const bool   accumulate     = false;
+    cv::Mat_<float>     result;
+    cv::calcHist(&plane, imageCount, 0, cv::noArray(), result,
+                 dimensionCount, histSizes, histRanges, uniform, accumulate);
+    return result;
+}
+
+// Normalize the histogram in h for display in rows.
+//
+static void normalizeHistogram(cv::Mat &h, int rows)
+{
+    static const double alpha = 0;
+    static const int normKind = cv::NORM_MINMAX;
+    static const int dtype = -1;
+    const double beta = rows;
+    cv::normalize(h, h, alpha, beta, normKind, dtype, cv::noArray());
+}
+
+// Draw normalized histogram in color on image.
+//
+static void drawHistogram(const cv::Mat_<double> &histogram, int binWidth,
+                          const cv::Scalar &color, cv::Mat &image)
+{
+    cv::Point p0(0, image.rows - cvRound(histogram(0)));
+    for (int i = 1; i < histogram.cols; ++i) {
+        static const int thickness = 2;
+        static const int lineType = 8;
+        static const int shift = 0;
+        const cv::Point p1(i * binWidth, image.rows - cvRound(histogram(i)));
+        cv::line(image, p0, p1, color, thickness, lineType, shift);
+        p0 = p1;
+    }
+}
+
+// Return a new image with a histogram of colors in image.
+//
 static cv::Mat computeHistogram(const cv::Mat &image)
 {
     enum Color { BLUE, GREEN, RED };
@@ -41,27 +87,13 @@ static cv::Mat computeHistogram(const cv::Mat &image)
     cv::Mat_<float> histogram[colorCount];
     cv::split(image, plane);
     for (int c = 0; c < colorCount; ++c) {
-        static const int    imageCount     = 1;
-        static const int    dimensionCount = 1;
-        static const int    histSizes[]    = { histSize };
-        static const float  histRange[]    = { 0, histSize };
-        static const float *histRanges[]   = { histRange };
-        static const bool   uniform        = true;
-        static const bool   accumulate     = false;
-        cv::calcHist(&plane[c], imageCount, 0, cv::noArray(), histogram[c],
-                     dimensionCount, histSizes, histRanges,
-                     uniform, accumulate);
+        histogram[c] = calculatePlane(plane[c], histSize);
     }
-    const int binWidth = cvRound(1.0 * image.cols / histSize);
-    cv::Mat result = cv::Mat_<cv::Vec3b>::zeros(image.rows, image.cols);
     for (int c = 0; c < colorCount; ++c) {
-        static const double alpha = 0;
-        static const int normKind = cv::NORM_MINMAX;
-        static const int dtype = -1;
-        const cv::Mat_<float> &h = histogram[c];
-        const double beta = result.rows;
-        cv::normalize(h, h, alpha, beta, normKind, dtype, cv::noArray());
+        normalizeHistogram(histogram[c], image.rows);
     }
+    cv::Mat result = cv::Mat_<cv::Vec3b>::zeros(image.rows, image.cols);
+    const int binWidth = cvRound(1.0 * result.cols / histSize);
     for (int c = 0; c < colorCount; ++c) {
         const cv::Mat_<float> &h = histogram[c];
         cv::Point p0(0, result.rows - cvRound(h(0)));
