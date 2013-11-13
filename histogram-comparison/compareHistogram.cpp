98b5eaac7ba08f57e7bfefafc59052a1b05f4c92
Tom Lyons <tbl3rd@gmail.com>
2013-11-13 16:13:41 -0500

    Checkpoint factored histogram comparison tutorial.

--- histogram-comparison/compareHistogram.cpp
+++ histogram-comparison/compareHistogram.cpp
@@ -1,6 +1,7 @@
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <iostream>
+#include <vector>
 
 
 // Create a new unobscured named window for image.
@@ -27,79 +28,112 @@ static void makeWindow(const char *window, const cv::Mat &image, int reset = 0)
     ++moveCount;
 }
 
-// Return an HSV encoding of the BGR image bgr.
+// Return a normalized Hue and Saturation histogram for the image hsv.
 //
-static cv::Mat bgrToHsv(const cv::Mat bgr)
+cv::MatND calculateHistogram(const cv::Mat &hsv)
 {
-    cv::Mat result;
-    cv::cvtColor(bgr, result, cv::COLOR_BGR2HSV);
+    cv::MatND result;
+    static const cv::Mat noMask;
+    static const int     hueBins        = 50;
+    static const int     satBins        = 60;
+    static const int     bins[]         = {hueBins, satBins};
+    static const float   hueRanges[]    = {0, 256};
+    static const float   satRanges[]    = {0, 180};
+    static const float  *ranges[]       = {hueRanges, satRanges};
+    static const int     imageCount     = 1;
+    static const int     channels[]     = {0, 1};
+    static const int     dimensionCount = 2;
+    static const bool    uniform        = true;
+    static const bool    accumulate     = false;
+    cv::calcHist(&hsv, imageCount, channels, noMask, result,
+                 dimensionCount, bins, ranges, uniform, accumulate);
+    static const double  alpha          = 0.0;
+    static const int     normKind       = cv::NORM_MINMAX;
+    static const int     dtype          = -1;
+    static const double  beta           = 1.0;
+    cv::normalize(result, result, alpha, beta, normKind, dtype, noMask);
     return result;
+}
+
+
+// Return a copy of the upper half of image.
+//
+static cv::Mat upperHalfOf(const cv::Mat &image)
+{
+    const int rows = image.rows;
+    const cv::Range highRows(0, rows / 2);
+    const cv::Range allCols(0, image.cols - 1);
+    cv::Mat result = image(highRows, allCols);
+    return result;
+}
+
+// Compare histogram[i] to histogram[0] for i = 0 to histogram.size().
+// Find the corresponding image names at name[i].
+//
+static void compareHistograms(std::vector<cv::MatND> &histogram,
+                              const char *name[])
+{
+    static const struct Method {const char *name; int value;} method[] = {
+        {"Correlation Match"     , CV_COMP_CORREL       },
+        {"Intersection Match"    , CV_COMP_INTERSECT    },
+        {"Chi-Square Distance"   , CV_COMP_CHISQR       },
+        {"Bhattacharyya Distance", CV_COMP_BHATTACHARYYA}
+    };
+    static const int methodCount = sizeof method / sizeof method[0];
+    std::cout << std::endl;
+    std::cout << "Match means higher value is more similar." << std::endl;
+    std::cout << "Distance means lower value is more similar." << std::endl;
+    for (int m = 0; m < methodCount; ++m) {
+        std::cout << std::endl << "Method: " << method[m].name << std::endl;
+        double result[histogram.size()];
+        for (int i = 0; i < histogram.size(); ++i) {
+            const int mv = method[m].value;
+            const double x = cv::compareHist(histogram[i], histogram[0], mv);
+            std::cout << "        " << name[i] << " to " << name[0] << ": "
+                      << x << std::endl;
+        }
+    }
+    std::cout << std::endl << "Done." << std::endl;
+}
+
+// Display the count images in bgr and compute their HSV histograms.
+// Then compare each histogram to the first one and report results.
+//
+static void showHistogramComparisons(int count,
+                                     const char *name[],
+                                     const cv::Mat bgr[])
+{
+    std::vector<cv::Mat> hsv(count);
+    std::vector<cv::MatND> histogram(count);
+    for (int i = 0; i < count; ++i) {
+        makeWindow(name[i], bgr[i]);
+        cv::cvtColor(bgr[i], hsv[i], cv::COLOR_BGR2HSV);
+    }
+    for (int i = 0; i < histogram.size(); ++i) {
+        histogram[i] = calculateHistogram(hsv[i]);
+    }
+    compareHistograms(histogram, name);
+    std::cout << "Press a key to quit." << std::endl;
+    cv::waitKey(0);
 }
 
 int main(int ac, const char *av[])
 {
-    static const int count = 3;
-    cv::Mat bgr[count], hsv[1 + count];
-    bool ok = ac == 1 + count;
+    bool ok = ac == 4;
     if (ok) {
-        for (int i = 0; i < count; ++i) {
+        static const char *name[] = { "Goal", "Tst0", "Tst1", "Half" };
+        static const int count = sizeof name / sizeof name[0];
+        cv::Mat bgr[count];
+        for (int i = 0; ok && i < count - 1; ++i) {
             bgr[i] = cv::imread(av[1 + i]);
             ok = ok && bgr[i].data;
         }
-    }
-    if (ok) for (int i = 0; i < count; ++i) hsv[i] = bgrToHsv(bgr[i]);
-    if (ok) {
-        const int rows = hsv[0].rows;
-        const cv::Range lowRows(rows / 2, rows - 1);
-        const cv::Range allCols(0, hsv[0].cols - 1);
-        hsv[3] = hsv[0](lowRows, allCols);
-    }
-    cv::MatND hist[1 + count];
-    static const int histCount = sizeof hist / sizeof hist[0];
-    if (ok) {
-        for (int i = 0; i < histCount; ++i) {
-            static const cv::Mat noMask;
-            static const int     hueBins        = 50;
-            static const int     satBins        = 60;
-            static const int     bins[]         = {hueBins, satBins};
-            static const float   hueRanges[]    = {0, 256};
-            static const float   satRanges[]    = {0, 180};
-            static const float  *ranges[]       = {hueRanges, satRanges};
-            static const int     imageCount     = 1;
-            static const int     channels[]     = {0, 1};
-            static const int     dimensionCount = 2;
-            static const bool    uniform        = true;
-            static const bool    accumulate     = false;
-            cv::calcHist(&hsv[i], imageCount, channels, noMask, hist[i],
-                         dimensionCount, bins, ranges, uniform, accumulate);
-            static const double  alpha          = 0.0;
-            static const int     normKind       = cv::NORM_MINMAX;
-            static const int     dtype          = -1;
-            static const double  beta           = 1.0;
-            cv::normalize(hist[i], hist[i], alpha, beta,
-                          normKind, dtype, noMask);
+        if (ok) {
+            bgr[count - 1] = upperHalfOf(bgr[0]);
+            showHistogramComparisons(count, name, bgr);
+            return 0;
         }
     }
-    if (ok) {
-        for (int m = 0; m < 4; ++m) {
-            double comparison[histCount] = {};
-            for (int i = 0; i < histCount; ++i) {
-                comparison[i] = cv::compareHist(hist[0], hist[i], m);
-                std::cout << "comparison[" << i << "] == "
-                          <<  comparison[i] << std::endl;
-            }
-            std::cout << "Method [" << m << "] "
-                      << "Perfect, Base-Half, Base-Test(1), Base-Test(2) :";
-            const char *separator = " "; 
-            for (int i = 0; i < histCount; ++i) {
-                std::cout << separator << comparison[i];
-                separator = ", ";
-            }
-            std::cout << std::endl << std::endl;
-        }
-        std::cout << "Done." << std::endl;
-    }
-    if (ok) return 0;
     std::cerr << av[0] << ": Demonstrate histogram comparison."
               << std::endl << std::endl
               << "Usage: " << av[0] << " <goal> <test0> <test1>" << std::endl
