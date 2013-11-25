739a33928f04caa592b71e725b7b5914bab8e361
Tom Lyons <tbl3rd@gmail.com>
2013-11-25 18:10:47 -0500

    Checkpoint tutorial on bounding polygonal contours.

--- bounding-contours/bound.cpp
+++ bounding-contours/bound.cpp
@@ -28,12 +28,12 @@ static void makeWindow(const char *window, const cv::Mat &image, int reset = 0)
 }
 
 
-// A display to demonstrate finding convex hull over contours.
+// A display to demonstrate bounding contours.
 //
 class DemoDisplay {
 
     const cv::Mat &srcImage;            // the original image
-    cv::Mat hullsImage;                 // contours in random colors
+    cv::Mat drawing;                    // bounds in random colors
 
     int bar;                            // position of threshold trackbar
     const int maxBar;                   // maximum value of trackbar
@@ -72,38 +72,51 @@ class DemoDisplay {
         return cv::Scalar(blue, green, red);
     }
 
-    // Draw contours in contour with hierarchy and convex hull around them
-    // over black hullsImage.
+    // Draw bounds around contours with hierarchy over black drawing.
     //
     // The reference implies that drawContours() ignores hierarchy when
     // maxLevel is 0 though. =tbl
     //
-    void drawHulls(const std::vector<std::vector<cv::Point> > &contour,
-                   const std::vector<cv::Vec4i> &hierarchy)
+    void drawBounds(const std::vector<std::vector<cv::Point> > &contour,
+                    const std::vector<cv::Vec4i> &hierarchy)
 
     {
         static const cv::Mat black
-            = cv::Mat::zeros(hullsImage.size(), hullsImage.type());
+            = cv::Mat::zeros(drawing.size(), drawing.type());
         static const bool orientClockwise = false;
         static const bool returnPoints = true;
-        static const int lineThickness = 1;
+        static const int polyThickness = 1;
+        static const int boundThickness = 2 * polyThickness;
         static const int lineType = 8;
         static const int maxLevel = 0;
+        static const double epsilon = 3.0;
+        static const bool closed = true;
         static const cv::Point offset(0, 0);
-        std::vector<std::vector<cv::Point> > hull(contour.size());
-        black.copyTo(hullsImage);
-        for (int i = 0; i < contour.size(); ++i) {
+        static const int shift = 0;
+        const int size = contour.size();
+        std::vector<std::vector<cv::Point> > polygon(size);
+        std::vector<cv::Rect> rect(size);
+        std::vector<cv::Point2f> center(size);
+        std::vector<float> radius(size);
+        for (int i = 0; i < size; ++i) {
+            cv::approxPolyDP(contour[i], polygon[i], epsilon, closed);
+            rect[i] = cv::boundingRect(polygon[i]);
+            cv::minEnclosingCircle(polygon[i], center[i], radius[i]);
+        }
+        black.copyTo(drawing);
+        for (int i = 0; i < size; ++i) {
             const cv::Scalar color = randomColor();
-            cv::convexHull(contour[i], hull[i], orientClockwise, returnPoints);
-            cv::drawContours(hullsImage, contour, i, color, lineThickness,
+            cv::drawContours(drawing, polygon, i, color, polyThickness,
                              lineType, hierarchy, maxLevel, offset);
-            cv::drawContours(hullsImage, hull, i, color, lineThickness,
-                             lineType, hierarchy, maxLevel, offset);
+            cv::rectangle(drawing, rect[i].tl(), rect[i].br(),
+                          color, boundThickness, lineType, shift);
+            cv::circle(drawing, center[i], radius[i], color,
+                       polyThickness, lineType, shift);
         }
     }
 
     // Find contours in srcImage, at threshold t less than max, and draw
-    // their convex hull in some random color on hullsImage.
+    // bounding circles and rectangles in some random color on drawing.
     //
     void apply(double t, double max)
     {
@@ -114,7 +127,7 @@ class DemoDisplay {
         std::vector<cv::Vec4i> hierarchy;
         const cv::Mat thresholds = detectThresholds(srcImage, t, max);
         cv::findContours(thresholds, contour, hierarchy, mode, method, offset);
-        drawHulls(contour, hierarchy);
+        drawBounds(contour, hierarchy);
     }
 
     // The callback passed to createTrackbar() where all state is at p.
@@ -125,7 +138,7 @@ class DemoDisplay {
         assert(pD->bar <= pD->maxBar);
         const double value = pD->bar;
         pD->apply(value, pD->maxBar);
-        cv::imshow("Hulls",  pD->hullsImage);
+        cv::imshow("Bounds",  pD->drawing);
     }
 
     // Add a trackbar with label of range 0 to max in bar.
@@ -141,16 +154,16 @@ public:
     //
     void operator()(void) { DemoDisplay::show(0, this); }
 
-    // Demonstrate finding and drawing a convex hull on image s.
+    // Demonstrate bounding polygonal contours with circles and rectangles.
     //
     DemoDisplay(const cv::Mat &s):
-        srcImage(s), hullsImage(s.size(), CV_8UC3),
+        srcImage(s), drawing(s.size(), CV_8UC3),
         bar(100), maxBar(std::numeric_limits<uchar>::max())
     {
         makeWindow("Original", srcImage, 2);
-        makeWindow("Hulls",    hullsImage);
+        makeWindow("Bounds",    drawing);
         makeTrackbar("Threshold:", "Original", &bar, maxBar);
-        makeTrackbar("Threshold:", "Hulls",    &bar, maxBar);
+        makeTrackbar("Threshold:", "Bounds",    &bar, maxBar);
         cv::imshow("Original", srcImage);
     }
 };
@@ -166,13 +179,13 @@ int main(int ac, const char *av[])
             return 0;
         }
     }
-    std::cerr << av[0] << ": Find a convex hull around contours."
+    std::cerr << av[0] << ": Demonstrate bounding polygonal contours."
               << std::endl << std::endl
               << "Usage: " << av[0] << " <image-file>" << std::endl
               << std::endl
               << "Where: <image-file> is the name of an image file."
               << std::endl << std::endl
-              << "Example: " << av[0] << " ../resources/lena.jpg"
+              << "Example: " << av[0] << " ../resources/jets.jpg"
               << std::endl << std::endl;
     return 1;
 }
