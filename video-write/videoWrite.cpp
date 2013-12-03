b4a45c641678120d56e9736e341dce028c83e7b6
Tom Lyons <tbl3rd@gmail.com>
2013-12-03 14:07:49 -0500

    Factor to clarify phases.

--- video-write/videoWrite.cpp
+++ video-write/videoWrite.cpp
@@ -64,7 +64,7 @@ struct CvVideoCapture: cv::VideoCapture {
             result[1] = ((code & 0x0000ff00) >>  8);
             result[2] = ((code & 0x00ff0000) >> 16);
             result[3] = ((code & 0xff000000) >> 24);
-            result[4] = 0;
+            result[4] = ""[0];
         }
         return result;
     }
@@ -81,82 +81,97 @@ struct CvVideoCapture: cv::VideoCapture {
     CvVideoCapture(): VideoCapture() {}
 };
 
+// Open ac VideoWriter files like vc named on av into vw.
+// Return true unless something goes wrong.
+//
+static bool openChannelFiles(int ac, char *av[],
+                             CvVideoCapture &vc, cv::VideoWriter vw[])
+{
+    static const bool isColor = true;
+    const int codec = vc.fourCcCodec();
+    const double fps = vc.framesPerSecond();
+    const cv::Size size = vc.frameSize();
+    for (int i = 0; i < ac; ++i) {
+        vw[i].open(av[i], codec, fps, size, isColor);
+    }
+    for (int i = 0; i < ac; ++i) if (!vw[i].isOpened()) return false;
+    return true;
+}
+
+// Separate the count channels of input into output.
+//
+static void separateChannels(int count, CvVideoCapture &input,
+                             cv::VideoWriter output[])
+{
+    while (true) {
+        cv::Mat inFrame;
+        input >> inFrame;
+        if (inFrame.empty()) break;
+        for (int color = 0; color < count; ++color) {
+            std::vector<cv::Mat> channel;
+            cv::split(inFrame, channel);
+            const cv::Mat black
+                = cv::Mat::zeros(channel[0].size(), channel[0].type());
+            for (int i = 0; i < channel.size(); ++i) {
+                if (i != color) channel[i] = black;
+            }
+            cv::Mat outFrame;
+            cv::merge(channel, outFrame);
+            output[color] << outFrame;
+        }
+    }
+}
+
+// Play the ac VideoCapture files named in av[].
+//
+struct VideoShow { const char *name; CvVideoCapture vc; cv::Mat frame; };
+static void playVideo(int ac, char *av[])
+{
+    bool ok = true;
+    std::vector<VideoShow> video(ac);
+    for (int i = 0; ok && i < ac; ++i) {
+        video[i].name = av[i];
+        video[i].vc.open(video[i].name);
+        ok = ok && video[i].vc.isOpened();
+    }
+    if (ok) {
+        for (int i = 0; ok && i < ac; ++i) {
+            makeWindow(video[i].name, video[i].vc.frameSize(), i == 0? 2: 0);
+        }
+        const int msFrameDelay = 1.0 / video[0].vc.framesPerSecond() * 1000;
+        while (ok) {
+            for (int i = 0; ok && i < ac; ++i) {
+                video[i].vc >> video[i].frame;
+                ok = !video[i].frame.empty();
+                if (ok) cv::imshow(video[i].name, video[i].frame);
+            }
+            if (ok) {
+                const int c = cv::waitKey(msFrameDelay);
+                ok = c == -1;
+            }
+        }
+    }
+}
+
 int main(int ac, char *av[])
 {
     enum { BLUE, GREEN, RED, COUNT };
-    if (ac == 5) {
-        cv::VideoWriter out[COUNT];
+    if (ac == 2 + COUNT) {
+        cv::VideoWriter output[COUNT];
         CvVideoCapture input(av[1]);
         bool ok = input.isOpened();
+        if (ok) ok = openChannelFiles(ac - 2, av + 2, input, output);
         if (ok) {
-            const int codec = input.fourCcCodec();
-            const double fps = input.framesPerSecond();
-            const cv::Size frameSize = input.frameSize();
-            for (int i = 0; i < COUNT; ++i) {
-                static const bool isColor = true;
-                out[i].open(av[i + 2], codec, fps, frameSize, isColor);
-            }
-            for (int i = 0; ok && i < COUNT; ++i) ok = out[i].isOpened();
-        }
-        while (ok) {
-            cv::Mat inFrame;
-            input >> inFrame;
-            if (inFrame.empty()) break;
-            for (int color = BLUE; color < COUNT; ++color) {
-                std::vector<cv::Mat> channel;
-                cv::split(inFrame, channel);
-                const cv::Mat black
-                    = cv::Mat::zeros(channel[0].size(), channel[0].type());
-                for (int i = 0; i < channel.size(); ++i) {
-                    if (i != color) channel[i] = black;
-                }
-                cv::Mat outFrame;
-                cv::merge(channel, outFrame);
-                out[color] << outFrame;
-            }
-        }
-        if (ok) {
-            const double fps = input.framesPerSecond();
+            separateChannels(COUNT, input, output);
             std::cout << std::endl << av[0] << ": Press any key to quit."
                       << std::endl << std::endl
                       << input.frameCount() << " frames ("
                       << input.frameSize().width << " x "
                       << input.frameSize().height
                       << ") with codec " << input.fourCcCodecString()
-                      << " at " << fps << " frames/second."
-                      << std::endl << std::endl;
-            struct {
-                const char *name; CvVideoCapture vc; cv::Mat frame;
-            } video[4] = {
-                { .name = "Source" },
-                { .name = "Blue"   },
-                { .name = "Green"  },
-                { .name = "Red"    }
-            };
-            const int videoCount = sizeof video / sizeof video[0];
-            for (int i = 0; ok && i < videoCount; ++i) {
-                video[i].vc.open(av[1 + i]);
-                ok = ok && video[i].vc.isOpened();
-            }
-            if (ok) {
-                for (int i = 0; ok && i < videoCount; ++i) {
-                    const char *name = video[i].name;
-                    const cv::Size size = video[i].vc.frameSize();
-                    makeWindow(name, size, i == 0? 2: 0);
-                }
-                const int msFrameDelay = 1.0 / fps * 1000;
-                while (ok) {
-                    for (int i = 0; ok && i < videoCount; ++i) {
-                        video[i].vc >> video[i].frame;
-                        ok = !video[i].frame.empty();
-                        if (ok) cv::imshow(video[i].name, video[i].frame);
-                    }
-                    if (ok) {
-                        const int c = cv::waitKey(msFrameDelay);
-                        ok = c == -1;
-                    }
-                }
-            }
+                      << " at " << input.framesPerSecond()
+                      << " frames/second." << std::endl << std::endl;
+            playVideo(ac - 1, av + 1);
             return 0;
         }
     }
