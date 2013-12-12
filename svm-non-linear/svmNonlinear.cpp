96f97a4c846d36ff719e4dc49112e5cae8ca3f4c
Tom Lyons <tbl3rd@gmail.com>
2013-12-11 19:47:40 -0500

    Simplify again with implicit Scalar constructor from float.

--- svm-non-linear/svmNonlinear.cpp
+++ svm-non-linear/svmNonlinear.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <opencv2/core/core.hpp>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/ml/ml.hpp>
 
@@ -31,41 +29,35 @@ static void trainSvm(cv::SVM &svm, const cv::Mat &data, const cv::Mat labels)
     svm.train(data, labels, varIdx, sampleIdx, params);
 }
 
-// Return TOTAL points of mostly SEPARABLE training data.
+// Return COUNT points of mostly SEPARABLE training data.
 //
-static cv::Mat_<float> makeData(int TOTAL, const cv::Size &size)
+static cv::Mat_<float> makeData(int COUNT, const cv::Size &size)
 {
     static cv::RNG rng(666);
     static const int SEPARABLE = 90;
-    const int NONSEPARABLE = TOTAL - SEPARABLE;
+    const int NONSEPARABLE = COUNT - SEPARABLE;
     const int cols = size.width;
     const int rows = size.height;
-    cv::Mat_<float> result(TOTAL, 2, CV_32FC1);
-    cv::Mat class1 = result.rowRange(0, SEPARABLE);
-    rng.fill(class1.colRange(0, 1), cv::RNG::UNIFORM,
-             cv::Scalar(1), cv::Scalar(0.4 * cols));
-    rng.fill(class1.colRange(1, 2), cv::RNG::UNIFORM,
-             cv::Scalar(1), cv::Scalar(rows));
-    cv::Mat class2 = result.rowRange(NONSEPARABLE, TOTAL);
-    rng.fill(class2.colRange(0, 1), cv::RNG::UNIFORM,
-             cv::Scalar(0.6 * cols), cv::Scalar(cols));
-    rng.fill(class2.colRange(1, 2), cv::RNG::UNIFORM,
-             cv::Scalar(1), cv::Scalar(rows));
-    cv::Mat classX = result.rowRange(SEPARABLE, NONSEPARABLE);
-    rng.fill(classX.colRange(0, 1), cv::RNG::UNIFORM,
-             cv::Scalar(0.4 * cols), cv::Scalar(0.6 * cols));
-    rng.fill(classX.colRange(1, 2), cv::RNG::UNIFORM,
-             cv::Scalar(1), cv::Scalar(rows));
+    cv::Mat_<float> result(COUNT, 2, CV_32FC1);
+    const cv::Mat class1 = result.rowRange(0, SEPARABLE);
+    rng.fill(class1.colRange(0, 1), cv::RNG::UNIFORM, 1.0, 0.4 * cols);
+    rng.fill(class1.colRange(1, 2), cv::RNG::UNIFORM, 1.0, 1.0 * rows);
+    const cv::Mat class2 = result.rowRange(NONSEPARABLE, COUNT);
+    rng.fill(class2.colRange(0, 1), cv::RNG::UNIFORM, 0.6 * cols, 1.0 * cols);
+    rng.fill(class2.colRange(1, 2), cv::RNG::UNIFORM, 1.0       , 1.0 * rows);
+    const cv::Mat classX = result.rowRange(SEPARABLE, NONSEPARABLE);
+    rng.fill(classX.colRange(0, 1), cv::RNG::UNIFORM, 0.4 * cols, 0.6 * cols);
+    rng.fill(classX.colRange(1, 2), cv::RNG::UNIFORM, 1.0       , 1.0 * rows);
     return result;
 }
 
-// Return half the TOTAL data labeled 1.0 and half labeled 2.0.
+// Return half the COUNT data labeled 1.0 and half labeled 2.0.
 //
-static cv::Mat_<float> labelData(int TOTAL)
+static cv::Mat_<float> labelData(int COUNT)
 {
-    cv::Mat_<float> result(TOTAL, 1, CV_32FC1);
-    result.rowRange(        0, TOTAL / 2).setTo(1.0);
-    result.rowRange(TOTAL / 2, TOTAL    ).setTo(2.0);
+    cv::Mat_<float> result(COUNT, 1, CV_32FC1);
+    result.rowRange(        0, COUNT / 2).setTo(1.0);
+    result.rowRange(COUNT / 2, COUNT    ).setTo(2.0);
     return result;
 }
 
@@ -93,21 +85,21 @@ static void drawRegions(cv::Mat &image, const cv::SVM &svm)
     }
 }
 
-// Draw training data as TOTAL circles of radius 3 on image.
+// Draw training data as COUNT circles of radius 3 on image.
 // Again, draw class 1.0 in green and class 2.0 in blue.
 //
-static void drawData(cv::Mat &image, int TOTAL, const cv::Mat_<float> &data)
+static void drawData(cv::Mat &image, int COUNT, const cv::Mat_<float> &data)
 {
     static const cv::Scalar green(  0, 255,   0);
     static const cv::Scalar  blue(255,   0,   0);
     static const int radius = 3;
     static const int thickness = -1;
     static const int lineKind = 8;
-    for (int i = 0; i < TOTAL / 2; ++i) {
+    for (int i = 0; i < COUNT / 2; ++i) {
         const cv::Point center(data(i, 0), data(i, 1));
         cv::circle(image, center, radius, green, thickness, lineKind);
     }
-    for (int i = TOTAL / 2; i < TOTAL; ++i) {
+    for (int i = COUNT / 2; i < COUNT; ++i) {
         const cv::Point center(data(i, 0), data(i, 1));
         cv::circle(image, center, radius, blue, thickness, lineKind);
     }
@@ -134,16 +126,16 @@ static void drawSupportVectors(cv::Mat &image, const cv::SVM &svm)
 
 int main(int, const char *[])
 {
-    static const int TOTAL = 200;
+    static const int COUNT = 200;
     cv::Mat image = cv::Mat::zeros(512, 512, CV_8UC3);
-    const cv::Mat_<float> data = makeData(TOTAL, image.size());
-    const cv::Mat_<float> labels = labelData(TOTAL);
+    const cv::Mat_<float> data = makeData(COUNT, image.size());
+    const cv::Mat_<float> labels = labelData(COUNT);
     std::cout << "Training SVM ... " << std::flush;
     cv::SVM svm;
     trainSvm(svm, data, labels);
     std::cout << "done." << std::endl;
     drawRegions(image, svm);
-    drawData(image, TOTAL, data);
+    drawData(image, COUNT, data);
     drawSupportVectors(image, svm);
     cv::imwrite("result.png", image);
     cv::imshow("SVM for Non-Linear Training Data", image);
